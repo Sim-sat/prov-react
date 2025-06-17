5d17b8cf298d99b4ea5dded359a38e599195afb2
Simon Sattelberger <simon.sattelberger@com-sys.de>
2025-06-17 08:52:59 +0200

    added conditional vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-export default defineConfig({
-    plugins: [
-        react(),
-        tailwindcss()],
+export default defineConfig(({ command}) => {
+    if (command === 'serve') { //dev environment
+        return {
+            plugins: [
+                react(),
+                tailwindcss()],
+            server: {
+                proxy: {
+                    '/protocol': {
+                        target: 'http://localhost:8080',
+                        changeOrigin: true,
+                        secure: false,
+                    }
+                }
+            }
+        }
+    } else {
+        // build environment
+        return {
+            plugins: [
+                react(),
+                tailwindcss()],
+
+        }
+    }
 })
